c1dc0ac035f81062f01f027c6e233b9795f5bb93
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-08-31 14:40:41 +0530

    added basic tweet component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Tweet from "./component/tweet";
+
+function App() {
+  return (
+    <>
+      <Tweet content={"hello"} likes={2} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Tweet.jsx
@@ -0,0 +1,11 @@
+function Tweet({ content, likes }) {
+  return (
+    <>
+      <div>{content}</div>
+
+      <div>{likes}</div>
+    </>
+  );
+}
+
+export default Tweet;
